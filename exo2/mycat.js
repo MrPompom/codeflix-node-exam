906b7a1f46e78226becd966f23476946ac8f795a
bastien prompsy <bastienprompsy@gmail.com>
2020-11-18 14:13:54 +0100

    up and finish exo2

--- exo2/mycat.js
+++ exo2/mycat.js
@@ -1,8 +1,10 @@
+const { fstat } = require('fs');
+
   function mycat () {
-  console.log(process.argv)
-  if (process.argv.length < 3) {
     const readline = require('readline');
-    const rl = readline.createInterface({
+    const fs = require('fs')
+  if (process.argv.length < 3) {
+      const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout
     });
@@ -10,9 +12,26 @@
         console.log(name)
     });
   }
-  else {
-  console.log(process.argv[2])
+
+  else if (process.argv[2] === "-e") {
+      var datacopy;
+      let j = 0;
+      fs.readFile(process.argv[3], 'utf8', function(err, data) {
+        if (err) throw err;
+        const lines = data.split(/\r?\n/);
+        lines.forEach((line) => {
+            console.log(line + "$");
+        });
+
+    });
   }
+
+  else {
+        fs.readFile(process.argv[2], 'utf8', function(err, data) {
+        if (err) throw err;
+        console.log(data)
+    });
+    }
   }
 
   mycat();
